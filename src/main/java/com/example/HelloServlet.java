307041129ca89733a6091ac45a95c34ecde33f18
nazishanwar1 <129955360+nazishanwar1@users.noreply.github.com>
2025-09-16 07:45:48 +0500

    new-login

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -20,19 +20,8 @@ public class HelloServlet extends HttpServlet {
         out.println("<form method='POST' action='login'>");
         out.println("Username: <input type='text' name='username'><br><br>");
         out.println("Email: <input type='email' name='email'><br><br>");
-        out.println("<input type='submit' value='Submit'>");
+        out.println("<input type='submit' value='Login'>");
         out.println("</form>");
-        out.println("</body></html>");
-    }
-    
-   // Handle form submission (POST request)
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1 style='color:green;'>Hello! Welcome to My-WebApp</h1>");
         out.println("</body></html>");
     }
 
@@ -40,15 +29,18 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Get form values
         String username = request.getParameter("username");
         String email = request.getParameter("email");
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        // Show welcome message + user info
         out.println("<html><body>");
-        out.println("<h2>Login Successful</h2>");
-        out.println("<p>Welcome, <b>" + username + "</b></p>");
-        out.println("<p>Your email: <b>" + email + "</b></p>");
+        out.println("<h1 style='color:green;'>Hello! Welcome to My-WebApp</h1>");
+        out.println("<p><b>Username:</b> " + username + "</p>");
+        out.println("<p><b>Email:</b> " + email + "</p>");
         out.println("</body></html>");
     }
 }
