db312b6cb6eb34376ff9f9fe3d9852c0764d9265
nazishanwar1 <129955360+nazishanwar1@users.noreply.github.com>
2025-09-16 07:31:18 +0500

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,25 +1,3 @@
-#package com.example;
-
-#import java.io.*;
-#import javax.servlet.*;
-#import javax.servlet.http.*;
-#import javax.servlet.annotation.WebServlet;
-
-#@WebServlet("/hello")
-#public class HelloServlet extends HttpServlet {
-#    private static final long serialVersionUID = 1L;
-
-#    @Override
-#    protected void doGet(HttpServletRequest request, HttpServletResponse response)
- #           throws IOException, ServletException {
-  #      response.setContentType("text/html");
-   #     PrintWriter out = response.getWriter();
-    #    out.println("<h1>Hello Jenkins CI/CD with Tomcat!</h1>");
-	#out.println("<h1>This is my first webapp<h1>");
-	#			out.println("<h2>Hello LinuxDMZ!<h2>");
-	#			out.println("<h2>Welcome to our team!<h2>");
-    #}
-#}
 package com.example;
 
 import java.io.*;
